7bb18975578dce3bf1d117ec1e7a58888ddfc638
Rowa <jrowah@gmail.com>
2022-10-06 11:30:25 +0300

    charAt method

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
+const firstName = "James";
+const initialsIndex = 0;
 
+const nameInitial = firstName.charAt(initialsIndex);
+console.log(nameInitial); //J
+console.log(firstName.charAt(firstName.length - 1)); //s
