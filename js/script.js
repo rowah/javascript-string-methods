aeb57d5a78a5c28556e61bb8b46ba00878a32b2a
Rowa <jrowah@gmail.com>
2022-10-06 12:38:04 +0300

    charCodeAt method

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
+//charAt
 const firstName = "James";
 const initialsIndex = 0;
 
 const nameInitial = firstName.charAt(initialsIndex);
 console.log(nameInitial); //J
 console.log(firstName.charAt(firstName.length - 1)); //s
+
+//charCodeAt
+const nameInitialCode = firstName.charCodeAt(initialsIndex);
+console.log(
+  `The ASCII code for first letter of my first name is ${nameInitialCode}`
+);
+console.log(firstName.charCodeAt(firstName.length - 1));
+console.log(firstName.charCodeAt(8));
